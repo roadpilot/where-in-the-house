c3c4044fa644bc088d310a07a853215ea1f55e84
mdacht777 <mdacht777@gmail.com>
2020-11-01 14:54:55 -0700

    button event listeners

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
 	console.log('index.js is loaded ...')
+
+    document.getElementById("btn1").addEventListener("click", (e) => {
+    putAway()
+    });
+
+    document.getElementById("btn2").addEventListener("click", (e) => {
+    search()
+    });
+
+    document.getElementById("btn3").addEventListener("click", (e) => {
+    browse()
+    });
+
+    function putAway(){
+        
+    }
 });
 
 const BASE_URL = "http://localhost:3000"
