b9716986a7642d3b24469b88e5ee19a303f1a83a
mdacht777 <mdacht777@gmail.com>
2020-11-01 16:23:03 -0700

    test populating from items#show

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -33,6 +33,18 @@ document.addEventListener("DOMContentLoaded", () => {
         c.innerHTML = "search"
     }
     function browse(){
+        fetch(`${BASE_URL}/items`)
+        .then(function (response) {
+            // The API call was successful!
+            return response.text();
+        }).then(function (html) {
+            // This is the HTML from our response as a text string
+            console.log(html);
+            c.innerHTML = html
+        }).catch(function (err) {
+            // There was an error
+            console.warn('Something went wrong.', err);
+        })
     }
 
 });
