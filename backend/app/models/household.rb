72a544b16fbab45a2613accaa8743531fc2bfaa2
mdacht777 <mdacht777@gmail.com>
2020-11-01 09:21:51 -0700

    test associations

--- backend/app/models/household.rb
+++ backend/app/models/household.rb
@@ -1,2 +1,5 @@
 class Household < ApplicationRecord
+    has_many :items 
+    has_many :household_users
+    has_many :users, through: :household_users
 end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
+    has_many :household_users
+    has_many :households, through: :household_users
 end
