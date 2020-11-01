38d4866bc80ac13171845e4bb1bfbe4dd6830f96
mdacht777 <mdacht777@gmail.com>
2020-10-31 22:28:13 -0600

    items migrate

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_01_040427) do
+ActiveRecord::Schema.define(version: 2020_11_01_042642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,17 @@ ActiveRecord::Schema.define(version: 2020_11_01_040427) do
   end
 
   create_table "households", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "current_location"
+    t.string "proper_location"
+    t.integer "household_id"
+    t.integer "last_update_user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
