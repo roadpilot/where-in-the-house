3f66e6df2dee66668ccaf50c86c5f11d187a6dff
mdacht777 <mdacht777@gmail.com>
2020-11-01 13:35:39 -0700

    js loads test

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+	console.log('index.js is loaded ...')
+});
+
 const BASE_URL = "http://localhost:3000"
-const TRAINERS_URL = `${BASE_URL}/trainers`
-const POKEMONS_URL = `${BASE_URL}/pokemons`
+
