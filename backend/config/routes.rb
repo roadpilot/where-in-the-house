26462d396920220d609171631939db68f2ddd2ee
mdacht777 <mdacht777@gmail.com>
2020-11-01 20:26:49 -0700

    login route

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :household_users
   resources :households
   resources :users
+  get 'user/login' => 'users#login'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
