d81bb3d744092e846735277c7dba6cadda0369e4
mdacht777 <mdacht777@gmail.com>
2020-11-04 12:22:34 -0700

    update seeds for non-user household structure

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -7,28 +7,28 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.destroy_all
-u1 = User.create(
-username: Faker::Internet.username,
-password: "pw" 
-)
-puts "Created user: #{u1.username}"
+# u1 = User.create(
+# username: Faker::Internet.username,
+# password: "pw" 
+# )
+# puts "Created user: #{u1.username}"
 
-u2 = User.create(
-username: Faker::Internet.username,
-password: "pw" 
-)
-puts "Created user: #{u2.username}"
+# u2 = User.create(
+# username: Faker::Internet.username,
+# password: "pw" 
+# )
+# puts "Created user: #{u2.username}"
 
 Household.destroy_all
-h1 = Household.create()
-puts "Created household_id: #{h1.id}"
+# h1 = Household.create()
+# puts "Created household_id: #{h1.id}"
 
-HouseholdUser.destroy_all
-HouseholdUser.create(user_id: u1.id, household_id: h1.id)
-HouseholdUser.create(user_id: u2.id, household_id: h1.id)
+# HouseholdUser.destroy_all
+# HouseholdUser.create(user_id: u1.id, household_id: h1.id)
+# HouseholdUser.create(user_id: u2.id, household_id: h1.id)
 
 Item.destroy_all
 Item.create(household_id: h1.id, name: "sunglasses", description: "brown with camo temples", current_location: "closet shelf", proper_location: "")
 Item.create(household_id: h1.id, name: "", description: "it's blue and looks like a can opener", current_location: "kitchen utensil drawer", proper_location: "")
-Item.create(household_id: h1.id, name: "glass canning car", description: "", current_location: "basement storage room shelf", proper_location: "")
+Item.create(household_id: h1.id, name: "glass canning jar", description: "", current_location: "basement storage room shelf", proper_location: "")
 Item.create(household_id: h1.id, name: "flat head screw driver", description: "", current_location: "", proper_location: "tool box")
