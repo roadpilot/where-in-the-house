fb2f2fbe837e7f70cfc0e847ef73a3a6372ddc30
mdacht777 <mdacht777@gmail.com>
2020-11-05 15:40:19 -0700

    update route

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
 namespace :api do
   namespace :v1 do
-    resources :items, only: [:index, :create]
+    resources :items, only: [:index, :create, :update]
     # resources :household_users
     # resources :households
     # resources :users
