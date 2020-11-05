e4d5829952f7017ac37832da1b4ca4321d8654a5
mdacht777 <mdacht777@gmail.com>
2020-11-05 16:48:07 -0700

    serializer

--- backend/app/serializers/item_serializer.rb
+++ backend/app/serializers/item_serializer.rb
@@ -1,3 +1,4 @@
 class ItemSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :current_location, :proper_location, :household_id, :last_update_user_id
+  has_many :locations
 end
