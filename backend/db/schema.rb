bbe75b678ab801e1e3b7abb3183c787b86c91923
mdacht777 <mdacht777@gmail.com>
2020-11-05 16:37:18 -0700

    schema

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_02_184820) do
+ActiveRecord::Schema.define(version: 2020_11_05_042642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(version: 2020_11_02_184820) do
     t.string "proper_location"
     t.integer "household_id"
     t.integer "last_update_user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string "name"
+    t.integer "item_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
