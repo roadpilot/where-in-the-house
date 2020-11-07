4a122dd33f56e110fad916bc19be237e004e0071
mdacht777 <mdacht777@gmail.com>
2020-11-06 20:39:18 -0700

    route for locations index

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 namespace :api do
   namespace :v1 do
     resources :items, only: [:index, :create, :update]
+    resources :locations, only: [:index]
     # resources :household_users
     # resources :households
     # resources :users
